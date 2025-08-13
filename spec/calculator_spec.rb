80cf014e0ed46e5478ac5c4734c4a5fb4b0294e2
Lucas Hein <lucasrafaelhein365@gmail.com>
2025-08-13 20:08:10 -0300

    Calculator Teste

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -4,8 +4,29 @@ describe Calculator do #Describe a Calculator class, Descreve a classe Calculato
   context "when adding two numbers" do #Contexto do Teste
     it "should add 2 negative numbers correctly" do # Teste para adicionar dois números negativos
       calculator = Calculator.new # setup
-      result = calculator.add(-2, 3) # exercise
-      expect(result).to eq(1) # verify
+      result = calculator.add(0, 0) # exercise
+      expect(result).to eq(0) # verify
+    end
+  end
+  context "when subtracting two numbers" do #Contexto do Teste
+    it "should subtract 2 negative numbers correctly" do # Teste para subtrair dois números negativos
+      calculator = Calculator.new # setup
+      result = calculator.subtract(0, 0) # exercise
+      expect(result).to eq(0) # verify
+    end
+  end
+  context "when multiplying two numbers" do #Contexto do Teste
+    it "should multiply 2 negative numbers correctly" do # Teste para multiplicar dois números negativos
+      calculator = Calculator.new # setup
+      result = calculator.multiply(0, 0) # exercise
+      expect(result).to eq(0) # verify
+    end
+  end
+  context "when dividing two numbers" do #Contexto do Teste 
+    it "should divide 2 negative numbers correctly" do # Teste para dividir dois números negativos
+      calculator = Calculator.new # setup
+      result = calculator.divide(0, 1) # exercise
+      expect(result).to eq(0) # verify
     end
   end
 end
